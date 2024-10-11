257647cd5f985e8c64f0593601c8aa8c6d2c8f49
RocketRace <oliviaspalmu@gmail.com>
2024-10-11 16:30:55 +0300

    Skeleton shader code

--- docs/src.js
+++ docs/src.js
@@ -1,38 +1,53 @@
 "use strict";
 var _a;
-// assumed present
-const canvas = document.querySelector("#canvas");
+// JS utilities
 const panic = (msg) => {
     alert(msg);
     throw new Error(msg);
 };
-const gl = (_a = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("webgl")) !== null && _a !== void 0 ? _a : panic("Unable to initialize WebGL. Your browser or machine may not support it.");
-// utility functions
+// WebGL utilities
 const compileShader = (shaderSource, shaderType) => {
     const shader = gl.createShader(shaderType);
     gl.shaderSource(shader, shaderSource);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        gl.deleteShader(shader);
         panic(`Could not compile shader: ${gl.getShaderInfoLog(shader)}`);
     }
     return shader;
 };
-const createProgram = (vertexShaderSource, fragmentShaderSource) => {
-    const vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER);
-    const fragmentShader = compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
-    const shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        panic(`Could not link program: ${gl.getProgramInfoLog(shaderProgram)}`);
+const createProgram = (vsSource, fsSource) => {
+    const vs = compileShader(vsSource, gl.VERTEX_SHADER);
+    const fs = compileShader(fsSource, gl.FRAGMENT_SHADER);
+    const program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        gl.deleteProgram(program);
+        panic(`Could not link program: ${gl.getProgramInfoLog(program)}`);
     }
-    return shaderProgram;
+    return program;
 };
-// Set the clear color to full opaque black
+// Shader code
+const vsSource = `
+attribute vec4 aVertexPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+`;
+const fsSource = `
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
+// Main program
+// Initialize the canvas and context
+const canvas = document.querySelector("#canvas");
+const gl = (_a = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("webgl")) !== null && _a !== void 0 ? _a : panic("Unable to initialize WebGL. Your browser or machine may not support it.");
+// Clear with all black
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
-// Clear with the specified color
 gl.clear(gl.COLOR_BUFFER_BIT);
-const vertexShaderSource = ``;
-const fragmentShaderSource = ``;
-const program = createProgram(vertexShaderSource, fragmentShaderSource);
+const program = createProgram(vsSource, fsSource);
--- src.ts
+++ src.ts
@@ -1,45 +1,60 @@
-// assumed present
-const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
-
+// JS utilities
 const panic = (msg: string) => {
     alert(msg);
     throw new Error(msg)
 }
 
-const gl = canvas?.getContext("webgl") ?? panic(
-    "Unable to initialize WebGL. Your browser or machine may not support it."
-);
-
-// utility functions
-
+// WebGL utilities
 const compileShader = (shaderSource: string, shaderType: number) => {
     const shader = gl.createShader(shaderType)!;  
     gl.shaderSource(shader, shaderSource);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      panic(`Could not compile shader: ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        panic(`Could not compile shader: ${gl.getShaderInfoLog(shader)}`);
     }
     return shader;
 }
-const createProgram = (vertexShaderSource: string, fragmentShaderSource: string) => {
-    const vertexShader = compileShader(vertexShaderSource, gl.VERTEX_SHADER);
-    const fragmentShader = compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER);
-    const shaderProgram = gl.createProgram()!;
-    gl.attachShader(shaderProgram, vertexShader);
-    gl.attachShader(shaderProgram, fragmentShader);
-    gl.linkProgram(shaderProgram);
-    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        panic(`Could not link program: ${gl.getProgramInfoLog(shaderProgram)}`);
+const createProgram = (vsSource: string, fsSource: string) => {
+    const vs = compileShader(vsSource, gl.VERTEX_SHADER);
+    const fs = compileShader(fsSource, gl.FRAGMENT_SHADER);
+    const program = gl.createProgram()!;
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        gl.deleteProgram(program);
+        panic(`Could not link program: ${gl.getProgramInfoLog(program)}`);
     }
-    return shaderProgram;
+    return program;
 }
 
-// Set the clear color to full opaque black
+
+// Shader code
+const vsSource = `
+attribute vec4 aVertexPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+`;
+const fsSource = `
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
+
+// Main program
+
+// Initialize the canvas and context
+const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
+const gl = canvas?.getContext("webgl") ?? panic(
+    "Unable to initialize WebGL. Your browser or machine may not support it."
+);
+
+// Clear with all black
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
-// Clear with the specified color
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-const vertexShaderSource = ``;
-const fragmentShaderSource = ``;
-
-const program = createProgram(vertexShaderSource, fragmentShaderSource);
+const program = createProgram(vsSource, fsSource);
