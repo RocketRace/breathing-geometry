3993f12a9c5e08856c56abb16d60f15526554b52
RocketRace <oliviaspalmu@gmail.com>
2024-10-11 16:12:44 +0300

    Basic canvas

--- docs/src.js
+++ docs/src.js
@@ -1,4 +1,15 @@
 "use strict";
-const node = document.createElement("p");
-node.innerText = "Hello from JavaScript!";
-document.body.appendChild(node);
+var _a;
+// assumed present
+const canvas = document.querySelector("#canvas");
+const panic = (msg) => {
+    alert(msg);
+    throw new Error(msg);
+};
+const gl = (_a = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("webgl")) !== null && _a !== void 0 ? _a : panic("Unable to initialize WebGL. Your browser or machine may not support it.");
+// Set the clear color to full opaque black
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+// Clear with the specified color
+gl.clear(gl.COLOR_BUFFER_BIT);
+const shaderProgram = gl.createProgram();
+gl.linkProgram(shaderProgram);
--- src.ts
+++ src.ts
@@ -1,3 +1,19 @@
-const node = document.createElement("p");
-node.innerText = "Hello from JavaScript!";
-document.body.appendChild(node);
+// assumed present
+const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
+
+const panic = (msg: string) => {
+    alert(msg);
+    throw new Error(msg)
+}
+
+const gl = canvas?.getContext("webgl") ?? panic(
+    "Unable to initialize WebGL. Your browser or machine may not support it."
+);
+
+// Set the clear color to full opaque black
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+// Clear with the specified color
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const shaderProgram = gl.createProgram()!;
+gl.linkProgram(shaderProgram);
